f1611400dd67cba5322e3e67df14dff1f68fb628
siddu0426 <msvssiddhartha@gmail.com>
2025-09-06 06:20:53 +0530

    added express-rate-limiter

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const rateLimit = require('express-rate-limit');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
